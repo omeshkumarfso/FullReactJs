28f6724547c4e80e4f53a01a45f0da3cb6fba452
omeshkumarfso <omeshkumarfso@gmail.com>
2021-12-14 14:33:47 +0530

    ExpensesFilter Complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
+import React,{ useState } from "react";
 import Expenses from "./components/Expenses/Expenses.js";
 import NewExpense from "./components/NewExpense/NewExpense.js";
 // import Test from "./test/Test.js";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1", title: "Car Insurance", amount: 295, date: new Date(2021,2,28)
+  },
+  {
+    id: "e2", title: "Car Insurance", amount: 294, date: new Date(2020,2,27) 
+  },
+  {
+    id: "e3", title: "Car Insurance", amount: 293, date: new Date(2021,2,26) 
+  },
+  {
+    id: "e4", title: "Car Insurance", amount: 292, date: new Date(2022,2,25) 
+  },
+]
+
 function App() {
 
-  const expenses = [
-    {
-      id: "e1", title: "Car Insurance", amount: 295, date: new Date(2021,2,28)
-    },
-    {
-      id: "e2", title: "Car Insurance", amount: 294, date: new Date(2021,2,27) 
-    },
-    {
-      id: "e3", title: "Car Insurance", amount: 293, date: new Date(2021,2,26) 
-    },
-    {
-      id: "e4", title: "Car Insurance", amount: 292, date: new Date(2021,2,25) 
-    },
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("in App.js", expense)
-  }
+   setExpenses((prevExpenses) => {
+     return [expense, ...prevExpenses];
+   });
+  };
 
   return (
     <div>
